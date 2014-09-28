6e2ee6662236ae0147a6b2fb08f118aed04326e2
Kel Maluenda <novhz0514@gmail.com>
2014-09-27 18:35:51 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Mark Up / Demo for imageUploader </title>
+</head>
+<body>
+
+<form action="<?php basename(__FILE__);?>" method="POST" enctype="multipart/form-data">
+<input type="file" name="imgfile[]" multiple="multiple" id="imgfile" accept="image/*" required />
+<input type="submit" value="Upload Image" />
+</form>
+
+<?php
+include 'imgclass.php';
+$obj=new Foo;
+
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+{
+		$obj->upload_Image($_FILES['imgfile']['tmp_name'],$_FILES['imgfile']['name']);
+}
+
+}
+
+?>
+</body>
+</html>
